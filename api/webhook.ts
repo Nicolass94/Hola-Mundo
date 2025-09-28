b9306610e3196b86927f535e7c7e2f88058e3730
Nicolass94 <40880474+Nicolass94@users.noreply.github.com>
2025-09-28 03:12:32 -0300

    Delete api directory

--- api/webhook.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("Webhook recibido:", req.body);
-  res.status(200).json({ success: true });
-}
