98092dc01203e39763629b136fc7af7732a600e3
Nicolass94 <40880474+Nicolass94@users.noreply.github.com>
2025-09-28 22:07:45 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 // app/layout.tsx
+import React from "react";
+
 export const metadata = {
   title: "Hola Mundo MiniApp",
   description: "Miniapp de prueba en Base",
@@ -11,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
