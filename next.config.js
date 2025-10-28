11255b14b4acdb4f6df034af2f00cda82aef9f2a
Nicolass94 <40880474+Nicolass94@users.noreply.github.com>
2025-10-28 02:28:31 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
+  async rewrites() {
     return [
       {
         source: '/.well-known/farcaster.json',
         destination: '/farcaster.json',
-        permanent: true,
       },
     ];
   },
